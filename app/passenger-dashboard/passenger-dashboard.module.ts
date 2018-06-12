212f1df5d801dbbc9ea798ecb61c06f0ae0c3ffe
Alexander Rojas <alexrrojas.b@gmail.com>
2018-06-12 10:34:33 -0600

    Feature-module routes with forChild()

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -8,6 +8,11 @@ import { PassengerDetailComponent } from "./components/passenger-detail/passenge
 import { PassengerDashboardService } from "./passenger-dashboard.service";
 import { PassengerViewerComponent } from "./containers/passenger-viewer/passenger-viewer.component";
 import { PassengerFormComponent } from "./components/passenger-form/passenger-form.component";
+import { RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [{
+    path: 'passengers', component: PassengerDashboardComponent
+}]
 
 @NgModule({
     declarations: [
@@ -20,11 +25,8 @@ import { PassengerFormComponent } from "./components/passenger-form/passenger-fo
     imports: [
         CommonModule,
         HttpModule,
-        FormsModule
-    ],
-    exports: [
-        // PassengerDashboardComponent,
-        PassengerViewerComponent
+        FormsModule,
+        RouterModule.forChild(routes)
     ],
     providers: [
         PassengerDashboardService
